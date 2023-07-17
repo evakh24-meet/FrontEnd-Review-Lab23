27e08ac2aa6a2d06f6d194a6daa485a2b4743868
evakh24-meet <139681303+evakh24-meet@users.noreply.github.com>
2023-07-17 14:10:53 +0300

    Updated version of my page about goats with bonus.

--- button.js
+++ button.js
@@ -1,3 +1,8 @@
+var count = 0
+
 function changebg() {
-	document.getElementById('html').style.backgroundColor = 'skyblue';
+	count++
+	if (count % 2 == 0){document.getElementById('html').style.backgroundColor = 'skyblue';}
+
+	else {document.getElementById('html').style.backgroundColor = 'palegreen';}
 }
